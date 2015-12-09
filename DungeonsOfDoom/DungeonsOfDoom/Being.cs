194a63c5e3ee86bff1082cb78a36aee9f0a5a0d5
AndreeaFC <andreea_vhr@yahoo.com>
2015-12-09 11:48:27 +0100

    -erased Monster and Player -added Being class

---
+++ DungeonsOfDoom/DungeonsOfDoom/Being.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonsOfDoom
+{
+    abstract class Being
+    {
+        public Being(string name, int health, int attackStrength)
+        {
+            Name = name;
+            Health = health;
+            AttackStrength = attackStrength;
+        }
+
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int AttackStrength { get; set; }
+    }
+	
+
+    abstract class Monster : Being
+    {
+        public Monster(string name, int health, int attackStrength, int fire, int ice) : base(name, health, attackStrength)
+        {
+            FireAttack = fire;
+            IceAttack = ice;
+        }
+        public int FireAttack { get; set; }
+        public int IceAttack { get; set; }
+    }
+
+    class Ogre : Monster
+    {
+        public Ogre() : base("Ogre", 40, 20, 100, 0)
+        {
+
+        }
+    }
+
+    class Giant : Monster
+    {
+        public Giant() : base("Giant", 50, 30, 0, 100)
+        {
+
+        }
+    }
+
+    class Cyclop : Monster
+    {
+        public Cyclop() : base("Cyclop", 60, 40, 50, 50)
+        {
+
+        }
+    }
+
+    class Player : Being
+    {
+        public Player(string name) : base(name, 100, 30)
+        {
+			Backpack = new List<Item>();
+        }
+        public int X { get; set; }
+        public int Y { get; set; }
+		public List<Item> Backpack { get; set; }
+	}
+
+}
